2b3e53135d1f01ba70620ce1bfc9b04b903b4119
Tht40 <77277019+Tht40@users.noreply.github.com>
2021-11-14 16:35:22 +0000

    menu image

--- menu.js
+++ menu.js
@@ -1,11 +1,16 @@
 function renderMenu(ctx){
+
     ctx.save();
+    
     ctx.drawImage(g_images.menuBackground, 40,40,800,800);
-
-
     //why dis dont work??
     ctx.font = "bold 30px Cursive";
     ctx.fillStyle = "blue";
-    ctx.fillText = ("START GAME", 0,0);
+    ctx.fillText = ("START GAME", 200,200);
+    ctx.strokeText("START GAME", 350, 200);
+    
+    
+    
+    
     ctx.restore();
 }
