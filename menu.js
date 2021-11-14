9aaad0aa7ca5fb2ad97c15e1986cbeaeb917c7bc
unknown <natidemisket@gmail.com>
2021-11-14 23:30:03 +0000

    more menu stuff

--- menu.js
+++ menu.js
@@ -8,8 +8,8 @@ var menuManager = {
     },
 
     //coordinates
-    optionCoords_x: 250,
-    optionCoords_y: 350,
+    menuItemCoords_x: 250,
+    menuItemCoords_y: 350,
 
     y_diff: 45,
     x_coord: 262,
@@ -22,7 +22,8 @@ var menuManager = {
 
     //parameters
     startGame: false,
-
+    isSelectCharMenu: false,
+    optionsMenu: false,
     drawMenuItem: function(ctx, text, x, y){
         ctx.fillStyle = objGradient;
         ctx.font = 'bold 50px Cursive';
@@ -30,13 +31,7 @@ var menuManager = {
         ctx.strokeText(text, x, y);
     },
 
-    renderMenu: function(ctx){
-
-        ctx.save();
-        
-        ctx.drawImage(g_images.menuBackground, 40,40,800,800);
-    
-        //why dis dont work??
+    drawMainMenu: function(ctx){
         objGradient = ctx.createLinearGradient(0,g_canvas.height*0.6, 0, 0);
         objGradient.addColorStop(0, 'blue');
       
@@ -44,9 +39,27 @@ var menuManager = {
         objGradient.addColorStop(3/3, 'yellow');
     
      
-        this.drawMenuItem(ctx,this.menuList.start, this.optionCoords_x, this.optionCoords_y);
-        this.drawMenuItem(ctx,this.menuList.selectChar, this.optionCoords_x, this.optionCoords_y+75);
-        this.drawMenuItem(ctx,this.menuList.options, this.optionCoords_x, this.optionCoords_y+150);
+        this.drawMenuItem(ctx,this.menuList.start, this.menuItemCoords_x, this.menuItemCoords_y);
+        this.drawMenuItem(ctx,this.menuList.selectChar, this.menuItemCoords_x, this.menuItemCoords_y+75);
+        this.drawMenuItem(ctx,this.menuList.options, this.menuItemCoords_x, this.menuItemCoords_y+150);
+
+    },
+    selectCharMenu: function(ctx){
+        //ctx.drawImage(g_images.spritesheet, 5, 7, 30, 58);
+    },
+
+    renderMenu: function(ctx){
+
+        ctx.save();
+        
+        ctx.drawImage(g_images.menuBackground, 40,40,800,800);
+        if(this.isSelectCharMenu){
+            this.selectCharMenu(ctx);
+        }else if (this.optionsMenu){
+            console.log("options menu")
+        }else{
+            this.drawMainMenu(ctx);
+        }
 
         ctx.restore();
         this.handleClicks();
@@ -54,11 +67,15 @@ var menuManager = {
 
     handleClicks: function(){
         if(g_mouseX >= this.x_coord){
-            if(g_mouseY >= this.start_coord_y && g_mouseY <= this.start_coord_y + this.y_diff){
-                    this.startGame = g_mouseClick === 1;
-                    console.log("startGame: "+this.startGame, g_mouseClick)
+            if(!this.isSelectCharMenu && !this.startGame && g_mouseY >= this.start_coord_y && g_mouseY <= this.start_coord_y + this.y_diff){
+                this.startGame = g_mouseClick === 1;
             }
-
+            if(!this.isSelectCharMenu && !this.startGame && g_mouseY >= this.selectChar_coord_y && g_mouseY <= this.selectChar_coord_y + this.y_diff){
+                if(g_mouseClick === 1) this.isSelectCharMenu = true
+            }
+            if(!this.isSelectCharMenu && !this.startGame && g_mouseY >= this.options_coord_y && g_mouseY <= this.options_coord_y + this.y_diff){
+                this.optionsMenu = g_mouseClick === 1
+            }
         }
     },
 }
